5c25c51cc86ea7ee476e766e1e4b94ba411f00a3
Ununhexium293 <quentin.lv04@gmail.com>
2024-10-25 22:41:12 +0200

    ajout de la structure tab_nom_recette qui stockera les recettes

---
+++ Data_gestion/source/struct.h
@@ -0,0 +1,31 @@
+#ifndef STRUCT_H
+#define STRUCT_H
+
+/*Tableau dynamique pour stocker les recettes et leur nombre*/
+typedef struct tab_nom_recette_{
+    char **tb_recette;
+    int nb_recette;
+    int taille_tab;
+}tab_nom_recette_t;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
