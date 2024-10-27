2e4c7b4f0b48df481d00c1b176799971814039a2
yanis <yanisrahoui@gmail.com>
2024-10-27 02:04:19 +0200

    4 struct ajoutées

--- Data_gestion/source/struct.h
+++ Data_gestion/source/struct.h
@@ -8,9 +8,32 @@ typedef struct tab_nom_recette_{
     int taille_tab;
 }tab_nom_recette_t;
 
+/*Tableau dynamique pour stocker les ingredients et leur nombre*/
+typedef struct tab_nom_ingredient_{
+    char **tb_ingredient;
+    int nb_ingredient;
+    int taille_tab;
+}tab_nom_ingredient_t;
 
+/*Structure contenant un ingrédient et sa quantité numerique associée*/
+typedef struct ingredient_quantite_{
+    int id_ingredient;
+    int quantite;
+}ingredient_quantite;
 
+/*Structure contenant un id de recette et un tableau d'ingredients et leur quantité avec*/
+typedef struct recette_ingredients_{
+    int id;
+    int taille_ingredient;
+    ingredient_quantite * liste_ingredient;
+}recette_ingredients;
 
+/*Structure contenant un tableau de recette, le nombre de recette et la taille du tableau pour les tableau dynamique*/
+typedef struct liste_recette_ingredients_{
+    int taille_tab;
+    int nb_recette;
+    recette_ingredients * tb_recette_ingredients;
+}liste_recette_ingredients;
 
 
 
