6254d1c7e5c017e56c258a8ff7a62c4385e2d30f
Ununhexium293 <quentin.lv04@gmail.com>
2024-10-28 08:40:17 +0100

    Data_gestion/source/struct.h -> modification du type tab_nom_recette_t pour le généraliser en tab_string_t
    Data_gestion/source/gestion_types.c -> modification des fonction pour aller avec le changement du type

--- Data_gestion/source/gestion_types.c
+++ Data_gestion/source/gestion_types.c
@@ -20,71 +20,88 @@ void cp_string(char *donnor, char *reciever){
     }
 }
 
+/*________________tab dynamique string____________________*/
 
-/*________________tab dynamique recette____________________*/
 
+/*initialise un tableau dynamique de string*/
+tab_string_t *init_tab_string(int size_tab){
+    tab_string_t *tab_string = malloc(sizeof(tab_string_t));
 
-/*initialise la structure*/
-tab_nom_recette_t *init_tab_nom_recette(int nb_recette){
-    tab_nom_recette_t *tab_nom = malloc(sizeof(tab_nom_recette_t));
+    char **tb_string = malloc(sizeof(char*) * (size_tab + 5)); //on ajoute quelques emplacemnet libre en plus si jamais on souhaite faire de petits ajouts
 
-    char **tab_recette = malloc(sizeof(char*) * (nb_recette + 5)); //on ajoute quelques emplacemnet libre en plus si jamais on souhaite faire de petits ajouts
-
-    if (tab_recette == NULL){
+    if (tb_string == NULL){
         exit(EXIT_FAILURE);
     }
 
-    tab_nom -> tb_recette = tab_recette;
-    tab_nom -> nb_recette = nb_recette;
-    tab_nom -> taille_tab = nb_recette + 5;
+    tab_string -> tb_string = tb_string;
+    tab_string -> nb_string = 0;
+    tab_string -> taille_tab = size_tab + 5;
 
-    return tab_nom;
+    return tab_string;
 }
 
 
 /*libere la memoire allouée au tableau dynamique*/
-void free_tab_nom_recette(tab_nom_recette_t *tab_nom){
+void free_tab_string(tab_string_t *tab_string){
 
-    /*pas sur d'en avoir besoin*/
-    for (int i = 0; i < tab_nom -> nb_recette; i++){
-        free(tab_nom -> tb_recette[i]);
+    for (int i = 0; i < tab_string -> nb_string; i++){
+        free(tab_string -> tb_string[i]);
     }
 
-    free(tab_nom -> tb_recette);
-    free(tab_nom);
+    free(tab_string -> tb_string);
+    free(tab_string);
 }
 
 
-/*ajoute une entrée dans dans le tableau tab_recette*/
-void add_tab_nom_recette(tab_nom_recette_t *tab_nom, char *nom_recette){
+/*ajoute une entrée dans dans le tableau tab_string*/
+void add_tab_string(tab_string_t *tab_string, char *string_e){
 
-    if (tab_nom -> nb_recette == tab_nom -> taille_tab){     //Si il n'y a plus de place dans le tableau, on double la taille de celui-ci et on ajoute ensuite ce que l'on voulais ajouter
+    if (tab_string -> nb_string == tab_string -> taille_tab){     //Si il n'y a plus de place dans le tableau, on double la taille de celui-ci
         
-        char **tab_recette = malloc(sizeof(char*) * (tab_nom -> taille_tab) * 2);
+        char **tb_string_sub = malloc(sizeof(char*) * (tab_string -> taille_tab) * 2);
 
-        if (tab_recette == NULL){
+        if (tb_string_sub == NULL){
             exit(EXIT_FAILURE);
         }
-
         
-        for (int i = 0; i < tab_nom -> taille_tab; i++){       //copie l'ancien tableau dans le nouveau
-            tab_recette[i] = tab_nom -> tb_recette[i];
+        for (int i = 0; i < tab_string -> taille_tab; i++){       //copie l'ancien tableau dans le nouveau
+            tb_string_sub[i] = tab_string -> tb_string[i];
         }
 
-        tab_nom -> taille_tab *= 2;
+        tab_string -> taille_tab *= 2;
 
-        free(tab_nom -> tb_recette);
-        tab_nom -> tb_recette = tab_recette;
+        free(tab_string -> tb_string);      //On libere la mémoir alloué à l'ancien tableau
+        tab_string -> tb_string = tb_string_sub;
     }
 
-    char *nom_recette_bis = malloc(sizeof(char) * len_string(nom_recette) + 1);
+    char *string_bis = malloc(sizeof(char) * len_string(string_e) + 1);
 
-    if (nom_recette_bis == NULL){
+    if (string_bis == NULL){
         exit(EXIT_FAILURE);
     }
 
-    cp_string(nom_recette, nom_recette_bis);
+    cp_string(string_e, string_bis);
 
-    tab_nom -> tb_recette[tab_nom -> nb_recette] = nom_recette_bis;
-    tab_nom -> nb_recette += 1;
+    tab_string -> tb_string[tab_string -> nb_string] = string_bis;
+    tab_string -> nb_string += 1;
+}
+
+
+/*supprime une entrée du tableau et avance les élément suivants*/
+int remove_tab_string(tab_string_t *tab_string, int indice){
+
+    if (indice >= tab_string -> nb_string){       //On verifie que l'element que l'on souhaite supprimer existe (pourras etre enlever plus tard / fais pour du debug principalement au cas où)
+        printf("Impossible de supprimer la chaine de charactere, indice trop grand.");
+        return -1;
+    }
+
+    free(tab_string -> tb_string[indice]);
+
+    for (int i = indice; i < tab_string -> nb_string - 1; i++){
+        tab_string -> tb_string[i] = tab_string -> tb_string[i + 1];
+    }
+
+    tab_string -> nb_string -= 1;
+
+    return 0;
 }
--- Data_gestion/source/struct.h
+++ Data_gestion/source/struct.h
@@ -1,16 +1,12 @@
 #ifndef STRUCT_H
 #define STRUCT_H
 
-/*Tableau dynamique pour stocker les recettes et leur nombre*/
-typedef struct tab_nom_recette_{
-    char **tb_recette;
-    int nb_recette;
+/*tableau dynamique de string*/
+typedef struct tab_string_{
+    char **tb_string;
+    int nb_string;
     int taille_tab;
-}tab_nom_recette_t;
-
-
-
-
+}tab_string_t;
 
 
 
