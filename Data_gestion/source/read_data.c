650d029243c664b3c011d96cd31c8591efac63e5
Ununhexium293 <quentin.lv04@gmail.com>
2024-11-08 16:37:31 +0100

    Data_gestion/source/read_data.c -> testé et approuvé + modif mineure dans ingredient.txt et recette.txt

--- Data_gestion/source/read_data.c
+++ Data_gestion/source/read_data.c
@@ -3,6 +3,8 @@
 #include "struct.h"
 #include "gestion_types.h"
 
+#define MAX_LEN_STRING 32
+
 #define DEBUG_read_data
 
 
@@ -29,9 +31,6 @@ tab_recette_ingredients_t *read_database(void){
         int nb_ingr;
         fscanf(file, "ing = nb %d : [", &nb_ingr);
 
-        printf("%d\n", nb_ingr);
-        fflush(stdout);
-
         tab_ingredients_t *tab_ingredient = init_tab_ingredient(nb_ingr);
 
         for (int j = 0; j < nb_ingr; j++){      //rempli les cases du tableau tab_ingredient
@@ -57,96 +56,89 @@ tab_string_t *read_recette(void){
     if(f==NULL){
         fclose(f);
         printf("[ERROR] file not found\n");
-        return;
+        fflush(stdout);
+        exit(EXIT_FAILURE);
     }
 
     int size_array;
-    fscanf(file,"elem = %d;\n",&size_array);
+    fscanf(f,"elem = %d;\n",&size_array);
 
     tab_string_t *tab_nom = init_tab_string(size_array);
 
     for (int i = 0; i < size_array; i++){
-        char *temp;
+        char temp[MAX_LEN_STRING];
 
-        fscanf("nom '%s'")
-    }
+        fscanf(f, "nom %s\n", temp);
 
-
-
-
-
-    int i = 0;
-
-    /*Desolé du while 1 mais je peux pas faire autrement vu que jveux pas fscanf(f,";\n") a chaque tour de boucle faut que je m'arrete
-    en plein milieu*/
-    while(1){
-        fscanf(f,"nom '%s'",&ListeNom->tb_string[i]);
-        i++;
-        if(feof(f)==0){
-            break;
-        }
-        fscanf(f,";\n");
+        add_tab_string(tab_nom, temp);
     }
 
     fclose(f);
-    return;    
+    return tab_nom;    
 }
 
-///*Charge les données du fichier inventaire.txt dans un tableau contenu dans la struct tab_inventaire*/
-//void read_inventaire(tab_ingredients_t * Inventaire){
-//    FILE * f = fopen("./Data_gestion/Data/inventaire.txt","r");
-//    if(f==NULL){
-//        fclose(f);
-//        printf("[ERROR] file not found\n");
-//        return;
-//    }
-//    int i = 0;
-//
-//    /*Desolé du while 1 mais je peux pas faire autrement vu que jveux pas fscanf(f,";\n") a chaque tour de boucle faut que je m'arrete
-//    en plein milieu*/
-//    while(1){
-//        fscanf(f,"ing=%d qt=%d",&Inventaire->tab_ingredient_quantite[i].id_ingredient,&Inventaire->tab_ingredient_quantite[i].quantite);
-//        i++;
-//        if(feof(f) == 0){
-//            break;
-//        }
-//        fscanf(f,";\n");
-//    }
-//    Inventaire->nb_ingredient = i;
-//    fclose(f);
-//    return;
-//}
-//
-///*Charge les données du fichier ingredient.txt dans un tableau contenu dans la struct tab_ingredient_nom_unite*/
-//void read_ingredient(tab_ingredient_nom_unite_t * ListeIng){
-//    FILE * f = fopen("./Data_gestion/Data/ingredient.txt","r");
-//    if(f==NULL){
-//        fclose(f);
-//        printf("[ERROR] file not found\n");
-//        return;
-//    }
-//    int i = 0;
-//    /*Toujours obligé de while 1 mais je peux vrm pas faire autrement*/
-//    while(1){
-//        fscanf(f,"qt=%c nom='%s'",&ListeIng->tab_ingredient_unite[i].unite,&ListeIng->tab_ingredient_unite[i].nom);
-//        i++;
-//        if(feof(f) == 0){
-//            break;
-//        }
-//        fscanf(f,";\n");
-//    }
-//    ListeIng->nb_ingredient = i;
-//    fclose(f);
-//    return;
-//}
-//
-//
+/*Charge les données du fichier inventaire.txt dans un tableau contenu dans la struct tab_inventaire*/
+tab_ingredients_t *read_inventaire(void){
+    FILE * f = fopen("./Data_gestion/Data/inventaire.txt","r");
+    if(f==NULL){
+        fclose(f);
+        printf("[ERROR] file not found\n");
+        exit(EXIT_FAILURE);
+    }
+
+    int size_array;
+    fscanf(f,"elem = %d;\n",&size_array);
+
+    tab_ingredients_t *tab_ingredient = init_tab_ingredient(size_array);
+
+    for (int i = 0; i < size_array; i++){
+        int id_ingr, quantity;
+        fscanf(f, "ing=%d qt=%d;\n", &id_ingr, &quantity);
+
+        add_tab_ingredient(tab_ingredient, id_ingr, quantity);
+    }
+
+    fclose(f);
+    return tab_ingredient;
+    
+}
+
+/*Charge les données du fichier ingredient.txt dans un tableau contenu dans la struct tab_ingredient_nom_unite*/
+tab_ingredient_nom_unite_t *read_ingredient(void){
+    FILE * f = fopen("./Data_gestion/Data/ingredient.txt","r");
+    if(f==NULL){
+        fclose(f);
+        printf("[ERROR] file not found\n");
+        exit(EXIT_FAILURE);
+    }
+
+    int size_array;
+    fscanf(f,"elem = %d;\n",&size_array);
+
+    tab_ingredient_nom_unite_t *tab_ingr = init_tab_ingredient_nom_unite(size_array);
+
+    for (int i = 0; i < size_array; i++){
+        char quantity;
+        char name[MAX_LEN_STRING];
+
+        fscanf(f, "qt=%c nom= %s\n", &quantity, name);
+
+        add_tab_ingredient_nom_unite(tab_ingr, name, quantity);
+    }
+
+    fclose(f);
+    return tab_ingr;
+}
+
+
+
+#ifdef DEBUG_read_data
 
 int main(void){
 
-    #ifdef DEBUG_read_data
+    /*Debug read_database (ok)*/
 
-    /*Debug read_recette (ok)*/
+    printf("Debug read_recette\n");
 
     tab_recette_ingredients_t *tab_recette = read_database();
     
@@ -164,9 +156,50 @@ int main(void){
 
     free_tab_recette(tab_recette);
 
-    /*Fin debug read_recette*/
+    /*Fin debug read_database*/
 
-    #endif
+    /*Debug read_nom*/
+
+    printf("\n\nDebug read_nom\n");
+
+    tab_string_t *tab_nom = read_recette();
+
+    for (int i = 0; i < tab_nom -> nb_string; i++){
+        printf("nom : %s\n", tab_nom -> tb_string[i]);
+    }
+
+    free_tab_string(tab_nom);
+
+    /*Fin debug read_nom*/
+
+    /*Debug read inventaire*/
+
+    printf("\n\nDebug read_inventaire\n\n");
+
+    tab_ingredients_t *tab_ingredient = read_inventaire();
+
+    for (int i = 0; i < tab_ingredient -> nb_ingredient; i++){
+        printf("id = %d, qt = %d\n", tab_ingredient -> tab_ingredient_quantite[i] -> id_ingredient, tab_ingredient -> tab_ingredient_quantite[i] -> quantite);
+    }
+
+    free_tab_ingredient(tab_ingredient);
+    /*Fin debug read_inventaire*/
+
+    /*Debug read_ingredients*/
+
+    printf("\n\nDebug read_ingredients\n");
+
+    tab_ingredient_nom_unite_t *tab_ingr_nom_unite = read_ingredient();
+
+    for (int i = 0; i < tab_ingr_nom_unite -> nb_ingredient; i++){
+        printf("qt = %c, nom : %s\n", tab_ingr_nom_unite -> tab_ingredient_unite[i] -> unite, tab_ingr_nom_unite -> tab_ingredient_unite[i] -> nom);
+    }
+
+    free_tab_ingredient_nom_unite(tab_ingr_nom_unite);
+
+    /*Fin debug read ingredients*/
 
     return 0;
 }
+
+#endif
